414ad5cc42eabc4afe690b4b4113c0579e60a46d
user1 <backenduz@gmail.com>
2023-02-15 22:23:20 +0500

    random pic of cat and dog

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 from time import sleep
-TOKEN = "1950937652:AAGHJLBvBMRJcaJ0qhmm6U9lmHiWbRAGC_8"
+TOKEN = "1950937652:AAEKFVSEwjT55RYro5X-3ILwKXnh_s6ThPU"
 
 def getUpd():
     URL = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
@@ -16,26 +16,41 @@ def getLastUpd(result):
     message_id = response['message']['message_id']
     return chat_id, text, update_id, message_id
 
-def sendPhoto(chat_id, text, photo, message_id):
-    params = {'chat_id': chat_id, 'photo': photo, 'reply_to_message_id': message_id}
-    
+def sendPhoto(chat_id, photo):
+
+    button1 = {'text':'Dog'}
+    button2 = {'text':'Cat'}
+    button3 = {'text':'Cow'}
+
+
+    keyboard = [[button1,button2],[button3]]
+    reply_markup = {'keyboard':keyboard, 'resize_keyboard': True}
+
+    payload = {
+        'chat_id':chat_id,
+        'photo':photo,
+        'reply_markup':reply_markup
+    }    
     URL = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
-    if text == '/dog':
-        r = requests.post(URL, params=params)
-        return r.json()
-    else: #boshqa text kelib qolganda xatolik bermasligi uchun if else yozdim.
-        pass
+
+    r = requests.post(URL, json=payload)
+    return r.json()
 
 lastUpdId = -1
-while True:
-    r = requests.get('https://random.dog/woof.json')
-    img_json = r.json()
-    img = img_json['url']
 
+while True:
     result = getUpd()
     chat_id, text, update_id, message_id = getLastUpd(result)
     print(message_id)
+    if text == 'Dog':
+        r = requests.get('https://random.dog/woof.json')
+        img_json = r.json()
+        img = img_json['url']
+    elif text == 'Cat':
+        r = requests.get('https://aws.random.cat/meow')
+        img_json = r.json()
+        img = img_json['file']
     if update_id != lastUpdId:
-        sendPhoto(chat_id, text, img, message_id)
+        sendPhoto(chat_id, img)
         lastUpdId = update_id
-    sleep(2)
+    sleep(1)
